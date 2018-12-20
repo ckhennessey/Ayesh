85e8cbeed5b5229168281f3cb32541b68403e6ea
ckhennessey <46029620+ckhennessey@users.noreply.github.com>
2018-12-20 16:57:05 -0500

    Add files via upload

--- ayeshDisplay.py
+++ ayeshDisplay.py
@@ -2,168 +2,106 @@
 
 import colorsys
 import math
-import time
+from random import randint
 
 import unicornhathd
 
 
-print("""Unicorn HAT HD: demo.py
+print("""Unicorn HAT HD: Candle
 
-This pixel shading demo transitions between 4 classic graphics demo effects.
+This example simulates a flickering candle flame.
 
 Press Ctrl+C to exit!
 
 """)
 
 unicornhathd.rotation(0)
-u_width, u_height = unicornhathd.get_shape()
+width, height = unicornhathd.get_shape()
+# buffer to contain candle "heat" data
+candle = [0] * 256
 
-# Generate a lookup table for 8bit hue to RGB conversion
-hue_to_rgb = []
+# create a palette for mapping heat values onto colours
+palette = [0] * 256
+for i in range(0, 256):
+    h = i / 5.0
+    h /= 360.0
+    s = (1.0 / (math.sqrt(i / 50.0) + 0.01))
+    s = min(1.0, s)
+    s = max(0.0, s)
 
-for i in range(0, 255):
-    hue_to_rgb.append(colorsys.hsv_to_rgb(i / 255.0, 1, 1))
+    v = i / 200.0
+    if i < 60:
+        v = v / 2
+    v = min(1.0, v)
+    v = max(0.0, v)
 
-
-def gradient(x, y, step):
-    g = x * 16
-    b = y * 16
-    r = 255 - (x * 16)
-    return (r, g, b)
-
-
-# twisty swirly goodness
-def swirl(x, y, step):
-    x -= (u_width / 2)
-    y -= (u_height / 2)
-    dist = math.sqrt(pow(x, 2) + pow(y, 2)) / 2.0
-    angle = (step / 10.0) + (dist * 1.5)
-    s = math.sin(angle)
-    c = math.cos(angle)
-    xs = x * c - y * s
-    ys = x * s + y * c
-    r = abs(xs + ys)
-    r = r * 12.0
-    r -= 20
-    return (r, r + (s * 130), r + (c * 130))
+    r, g, b = colorsys.hsv_to_rgb(h, s, v)
+    palette[i] = (int(r * 255.0), int(g * 255.0), int(b * 255.0))
 
 
-# roto-zooming checker board
-def checker(x, y, step):
-    x -= (u_width / 2)
-    y -= (u_height / 2)
-    angle = (step / 10.0)
-    s = math.sin(angle)
-    c = math.cos(angle)
-    xs = x * c - y * s
-    ys = x * s + y * c
-    xs -= math.sin(step / 200.0) * 40.0
-    ys -= math.cos(step / 200.0) * 40.0
-    scale = step % 20
-    scale /= 20
-    scale = (math.sin(step / 50.0) / 8.0) + 0.25
-    xs *= scale
-    ys *= scale
-    xo = abs(xs) - int(abs(xs))
-    yo = abs(ys) - int(abs(ys))
-    v = 0 if (math.floor(xs) + math.floor(ys)) % 2 else 1 if xo > .1 and yo > .1 else .5
-    r, g, b = hue_to_rgb[step % 255]
-    return (r * (v * 255), g * (v * 255), b * (v * 255))
+def set_pixel(b, x, y, v):
+    b[y * 16 + x] = int(v)
 
 
-# weeee waaaah
-def blues_and_twos(x, y, step):
-    x -= (u_width / 2)
-    y -= (u_height / 2)
-    scale = math.sin(step / 6.0) / 1.5
-    r = math.sin((x * scale) / 1.0) + math.cos((y * scale) / 1.0)
-    b = math.sin(x * scale / 2.0) + math.cos(y * scale / 2.0)
-    g = r - .8
-    g = 0 if g < 0 else g
-    b -= r
-    b /= 1.4
-    return (r * 255, (b + g) * 255, g * 255)
+def get_pixel(b, x, y):
+    # out of range sample lookup
+    if x < 0 or y < 0 or x >= 16 or y >= 16:
+        return 0
 
+    # subpixel sample lookup
+    if isinstance(x, float) and x < 15:
+        f = x - int(x)
+        return (b[int(y) * 16 + int(x)] * (1.0 - f)) + (b[int(y) * 16 + int(x) + 1] * (f))
 
-# rainbow search spotlights
-def rainbow_search(x, y, step):
-    xs = math.sin((step) / 100.0) * 20.0
-    ys = math.cos((step) / 100.0) * 20.0
-    scale = ((math.sin(step / 60.0) + 1.0) / 5.0) + 0.2
-    r = math.sin((x + xs) * scale) + math.cos((y + xs) * scale)
-    g = math.sin((x + xs) * scale) + math.cos((y + ys) * scale)
-    b = math.sin((x + ys) * scale) + math.cos((y + ys) * scale)
-    return (r * 255, g * 255, b * 255)
-
-
-# zoom tunnel
-def tunnel(x, y, step):
-    speed = step / 100.0
-    x -= (u_width / 2)
-    y -= (u_height / 2)
-    xo = math.sin(step / 27.0) * 2
-    yo = math.cos(step / 18.0) * 2
-    x += xo
-    y += yo
-    if y == 0:
-        if x < 0:
-            angle = -(math.pi / 2)
-        else:
-            angle = (math.pi / 2)
-    else:
-        angle = math.atan(x / y)
-    if y > 0:
-        angle += math.pi
-    angle /= 2 * math.pi  # convert angle to 0...1 range
-    hyp = math.sqrt(math.pow(x, 2) + math.pow(y, 2))
-    shade = hyp / 2.1
-    shade = 1 if shade > 1 else shade
-    angle += speed
-    depth = speed + (hyp / 10)
-    col1 = hue_to_rgb[step % 255]
-    col1 = (col1[0] * 0.8, col1[1] * 0.8, col1[2] * 0.8)
-    col2 = hue_to_rgb[step % 255]
-    col2 = (col2[0] * 0.3, col2[1] * 0.3, col2[2] * 0.3)
-    col = col1 if int(abs(angle * 6.0)) % 2 == 0 else col2
-    td = .3 if int(abs(depth * 3.0)) % 2 == 0 else 0
-    col = (col[0] + td, col[1] + td, col[2] + td)
-    col = (col[0] * shade, col[1] * shade, col[2] * shade)
-    return (col[0] * 255, col[1] * 255, col[2] * 255)
-
-
-def current_milli_time():
-    return int(round(time.time() * 1000))
-
-
-effects = [gradient, tunnel, rainbow_search, checker, swirl]
+    # fixed pixel sample lookup
+    return b[int(y) * 16 + int(x)]
 
 
 step = 0
 
 try:
     while True:
-        for i in range(100):
-            start = current_milli_time()
-            for y in range(u_height):
-                for x in range(u_width):
-                    r, g, b = effects[0](x, y, step)
-                    if i > 75:
-                        r2, g2, b2 = effects[-1](x, y, step)
-                        ratio = (100.00 - i) / 25.0
-                        r = r * ratio + r2 * (1.0 - ratio)
-                        g = g * ratio + g2 * (1.0 - ratio)
-                        b = b * ratio + b2 * (1.0 - ratio)
-                    r = int(max(0, min(255, r)))
-                    g = int(max(0, min(255, g)))
-                    b = int(max(0, min(255, b)))
-                    unicornhathd.set_pixel(x, y, r, g, b)
+        # step for waving animation, adds some randomness
+        step += randint(0, 15)
 
-            step += 2
+        # clone the current candle
+        temp = candle[:]
 
-            unicornhathd.show()
+        # seed new heat
+        v = 500
 
-        effect = effects.pop()
-        effects.insert(0, effect)
+        set_pixel(candle, 6, 15, v)
+        set_pixel(candle, 7, 15, v)
+        set_pixel(candle, 8, 15, v)
+        set_pixel(candle, 9, 15, v)
+        set_pixel(candle, 6, 14, v)
+        set_pixel(candle, 7, 14, v)
+        set_pixel(candle, 8, 14, v)
+        set_pixel(candle, 9, 14, v)
+
+        # blur, wave, and shift up one step
+        for x in range(0, 16):
+            for y in range(0, 16):
+                s = math.sin((y / 30.0) + (step / 10.0)) * ((16 - y) / 20.0)
+                v = 0
+                for i in range(0, 3):
+                    for j in range(0, 3):
+                        # r = randint(0, 2) - 1
+                        v += get_pixel(candle, x + i + s - 1, y + j)
+
+                v /= 10
+                set_pixel(temp, x, y, v)
+
+        candle = temp
+
+        # copy candle into UHHD with palette
+        for x in range(0, 16):
+            for y in range(0, 16):
+                o = (i * 3) + 1
+                r, g, b = palette[max(0, min(255, get_pixel(candle, x, y)))]
+                unicornhathd.set_pixel(x, y, r, g, b)
+
+        unicornhathd.show()
 
 except KeyboardInterrupt:
     unicornhathd.off()

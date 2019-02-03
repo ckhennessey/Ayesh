4356256636748eceeefc1a10d3aa461381b19b7c
ckhennessey <46029620+ckhennessey@users.noreply.github.com>
2019-02-02 23:50:02 -0500

    Update ayeshDisplay.py

--- ayeshDisplay.py
+++ ayeshDisplay.py
@@ -9,23 +9,18 @@ try:
 except ImportError:
     exit('This script requires the pillow module\nInstall with: sudo pip install pillow')
 
-try:
-    import unicornhathd as unicornhathd
-    print("unicorn hat hd detected")
-except ImportError:
-    from unicorn_hat_sim import unicornhathd as unicornhathd
-
+import unicornhathd
 
 
 print("""Unicorn HAT HD: Text
-    
-    This example shows how to draw, display and scroll text in a regular TrueType font on Unicorn HAT HD.
-    
-    It uses the Python Pillow/PIL image library, and all other drawing functions are available.
-    
-    See: http://pillow.readthedocs.io/en/3.1.x/reference/
-    
-    """)
+
+This example shows how to draw, display and scroll text in a regular TrueType font on Unicorn HAT HD.
+
+It uses the Python Pillow/PIL image library, and all other drawing functions are available.
+
+See: http://pillow.readthedocs.io/en/3.1.x/reference/
+
+""")
 
 # ========== Change the text you want to display, and font, here ================
 
@@ -83,7 +78,7 @@ draw.text((text_x, text_y), TEXT, fill=(255, 255, 255), font=font)
 # image, we subtract "width".
 for scroll in range(text_width - width):
     for x in range(width):
-        
+
         # Figure out what hue value we want at this point.
         # "x" is the position of the pixel on Unicorn HAT HD from 0 to 15
         # "scroll" is how far offset from the left of our text image we are
@@ -91,27 +86,27 @@ for scroll in range(text_width - width):
         # so we divide the pixel's position (x + scroll) by the total width of the text
         # If this pixel were half way through the text, it would result in the number 0.5 (180 degrees)
         hue = (x + scroll) / float(text_width)
-        
+
         # Now we need to convert our "hue" value into r,g,b since that's what colour space our
         # image is in, and also what Unicorn HAT HD understands.
         # This list comprehension is just a tidy way of converting the range 0.0 to 1.0
         # that hsv_to_rgb returns into integers in the range 0-255.
         # hsv_to_rgb returns a tuple of (r, g, b)
         br, bg, bb = [int(n * 255) for n in colorsys.hsv_to_rgb(hue, 1.0, 1.0)]
-        
+
         # Since our rainbow runs from left to right along the x axis, we can calculate it once
         # for every vertical line on the display, and then re-use that value 16 times below:
-        
+
         for y in range(height):
             # Get the r, g, b colour triplet from pixel x,y of our text image
             # Our text is white on a black background, so these will all be shades of black/grey/white
             # ie 255,255,255 or 0,0,0 or 128,128,128
             pixel = image.getpixel((x + scroll, y))
-            
+
             # Now we want to turn the colour of our text - shades of grey remember - into a mask for our rainbow.
             # We do this by dividing it by 255, which converts it to the range 0.0 to 1.0
             r, g, b = [float(n / 255.0) for n in pixel]
-            
+
             # We can now use our 0.0 to 1.0 range to scale our three colour values, controlling the amount
             # of rainbow that gets blended in.
             # 0.0 would blend no rainbow
@@ -120,14 +115,14 @@ for scroll in range(text_width - width):
             r = int(br * r)
             g = int(bg * g)
             b = int(bb * b)
-            
+
             # Finally we colour in our finished pixel on Unicorn HAT HD
             unicornhathd.set_pixel(width - 1 - x, y, r, g, b)
 
-# Finally, for each step in our scroll, we show the result on Unicorn HAT HD
+    # Finally, for each step in our scroll, we show the result on Unicorn HAT HD
     unicornhathd.show()
 
-# And sleep for a little bit, so it doesn't scroll too quickly!
+    # And sleep for a little bit, so it doesn't scroll too quickly!
     time.sleep(0.02)
 
 unicornhathd.off()
